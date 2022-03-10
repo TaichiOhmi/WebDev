fa3899b05930d7c4aee0c614633407a8c60cd926
TaichiOhmi <60906194+TaichiOhmi@users.noreply.github.com>
2022-03-10 23:18:36 +0900

    Day4

---
+++ Day4/index.php
@@ -0,0 +1,38 @@
+<?php
+
+    echo "hello world!<br>";
+    echo "Good evening!";
+    echo "<h1>Hi I am a message echoed in PHP</h1>";
+    echo "Mary" , "John","<br>";//concatenate
+    echo "My Name is "."Paul<br>";
+    echo "10000";
+    echo "<br>";
+    echo 10000 + 100;
+    $phoneNumber = "0809809";
+    $age = 20;
+    $name1="John";
+    $name2="Paul";
+    echo $name . " is not young anymore<br>";
+    echo $name1 . " " . $name2 . "<br>";
+    echo "$name1 $name2<br>";
+    echo '$name1 $name2';
+
+    $x = 1000;
+    $n = 26.900;
+    echo var_dump($x);
+    echo var_dump($n);
+    echo var_dump($name1);
+
+    
+    echo "100" + "10", "<br>";//110
+    echo "100" + 10, "<br>";//110
+    echo 100 + 10, "<br>";//110
+    echo "100" . "10", "<br>";//10010
+    echo "100" . 10, "<br>";//10010
+    echo 100 . 10, "<br>";//10010
+
+    
+
+
+
+?>
