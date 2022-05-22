72f8cdb331995cae3edc5c48d8e371f52b27aa11
TaichiOhmi <o6mi5.ta1@gmail.com>
2022-05-22 15:36:12 +0900

    Day49

--- Day049/home.blade.php
+++ Day049/home.blade.php
@@ -3,9 +3,10 @@
 @section('title', 'Home')
 @section('content')
     {{-- @dump($all_posts) --}}
+    {{-- @dump($suggestions) --}}
     <div class="row gx-5">
         <div class="col-8">
-            {{-- @if (Auth::user()->following->count() > 0 || Auth::user()->post->count() > 0) --}}
+            @if (Auth::user()->hasPost() || Auth::user()->posts->count() > 0)
                 @if ($all_posts->isNotEmpty())
                     @foreach ($all_posts as $post)
                         {{--   --}}
@@ -29,21 +30,71 @@
                         <a href="{{ route('post.create') }}" class="text-decoration-none">Shere your first photo</a>
                     </div>
                 @endif
-            {{-- @else --}}
+            @else
                 {{-- if there is not post yet --}}
-                {{-- <div class="text-center">
+                <div class="text-center">
                     <h2>Share Photos</h2>
                     <p class="text-muted">When you share photos, They'll appear on your wall</p>
                     <a href="{{ route('post.create') }}" class="text-decoration-none">Shere your first photo</a>
                 </div>
-            @endif --}}
+            @endif
         </div>
-        <div class="col-4 bg-secondary">
+        <div class="col-4">
             {{-- Profile Overview --}}
-            Profile Overview
+            <div class="row shadow-sm rounded p-1">
+                <div class="col-auto">
+                    <a href="{{ route('profile.show', Auth::user()->id) }}" class="text-decoration-none">
+                        @if (Auth::user()->avatar)
+                            <img src="{{ asset('/storage/images/'.Auth::user()->avatar) }}" alt="{{ Auth::user()->avatar }}" class="img-fluid img-thumbnail rounded-circle" width="50" height="" style="">
+                        @else
+                            <i class="fa-solid fa-circle-user text-secondary d-block text-center profile-icon" style="font-size:50px;"></i>
+                        @endif
+                    </a>
+                </div>
+                <div class="col d-flex align-items-center text-truncate">
+                    <a href="{{ route('profile.show', Auth::user()->id) }}" class="text-decoration-none text-dark">
+                        {{ Auth::user()->name }}
+                    </a>
+                </div>
+                <div class="col-auto"></div>
+            </div>
 
             {{-- Suggestions --}}
-            Suggestions
+            @isset($suggestions)
+                <div class="row mt-5">
+                    Suggestions for you
+                    <ul class="list-unstyled">
+                    @foreach ($suggestions as $suggestion)
+                        <li class="mt-2 d-flex">
+                            {{-- <div class="row">
+                                <div class="col-auto"> --}}
+                                    <a href="{{ route('profile.show', $suggestion->id) }}" class="text-decoration-none me-2">
+                                        @if ($suggestion->avatar)
+                                            <img src="{{ asset('/storage/images/'.$suggestion->avatar) }}" alt="{{ $suggestion->avatar }}" class="img-fluid img-thumbnail rounded-circle" width="50" height="50" style="">
+                                        @else
+                                            <i class="fa-solid fa-circle-user text-secondary d-block text-center" style="font-size:45px;"></i>
+                                        @endif
+                                    </a>
+                                {{-- </div>
+                                <div class="col d-flex align-items-center"> --}}
+                                    <a href="{{ route('profile.show', $suggestion->id) }}" class="text-decoration-none text-dark me-auto my-auto text-truncate" style="width:10rem;">
+                                        {{ $suggestion->name }}
+                                    </a>
+                                {{-- </div>
+                                <div class="col-auto d-flex align-items-center"> --}}
+                                    <form action="{{ route('follow.store', $suggestion->id) }}" method="post" class="ms-auto my-auto">
+                                        @csrf
+                                        <button type="submit" class="btn btn-sm btn-primary fw-bold">
+                                            Follow
+                                        </button>
+                                    </form>
+                                {{-- </div>
+                            </div> --}}
+                        </li>
+                    @endforeach
+                    </ul>
+                </div>
+            @endisset
         </div>
     </div>
 @endsection
